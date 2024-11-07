b4d36df62a2e9fb0a4f26269ca7f675383d54f10
KARAN GOPLANI <92224864+Karangoplani72@users.noreply.github.com>
2024-11-07 14:56:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listener for scrolling
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
